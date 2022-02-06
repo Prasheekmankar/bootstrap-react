e81b68251ed8e1cadefd75b39d5b9324b7f0fd16
Prasheekmankar <89675381+Prasheekmankar@users.noreply.github.com>
2022-02-06 15:08:46 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import { Row } from 'react-bootstrap';
+// import Container from 'react-bootstrap/Container'
+import { Col, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Row>
-     <div className='container'>
-       <div className='Row'>
-         <div className='Col'>
-           <p>aaaaaaaaaaaa aaaaaaaa aaaaaaaaaa aaaaaaaa</p>
-           <p>aaaaaaaaaaaa aaaaaaaa aaaaaaaaaa aaaaaaaa</p>
-           <p>aaaaaaaaaaaa aaaaaaaa aaaaaaaaaa aaaaaaaa</p>
-           <p>aaaaaaaaaaaa aaaaaaaa aaaaaaaaaa aaaaaaaa</p>
-
-         </div>
-       </div>
-     </div>
+        <Col className='one' lg={3} md={4} sm={6} xs={12} >   </Col>
+        <Col className='two' lg={3} md={4} sm={6} xs={12} >    </Col>
+        <Col className='three' lg={3} md={4} sm={6} xs={12} >    </Col>
+        <Col className='four' lg={3} md={4} sm={6} xs={12} > </Col>
      </Row>
+
+{/* <Container>
+  <Row>
+    <Col><div className='one'></div></Col>
+    <Col><div className='two'></div></Col>
+  </Row>
+  <Row>
+    <Col><div className='three'></div></Col>
+    <Col><div className='four'></div></Col>
+    </Row>
+</Container> */}
     </div>
   );
 }
