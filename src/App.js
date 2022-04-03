298bf0a2ce05e8d9bbcc32cc4976364955e74f84
Prasheekmankar <89675381+Prasheekmankar@users.noreply.github.com>
2022-04-03 22:38:00 +0530

    commit change

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-// import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container'
 import { Col, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+<Navbar bg="dark" variant="dark">
+    <Container>
+    <Nav className="me-auto">
+      <Nav.Link href="https://www.google.com/">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
       <Row>
         <Col className='one' lg={3} md={4} sm={6} xs={12} >   </Col>
         <Col className='two' lg={3} md={4} sm={6} xs={12} >    </Col>
         <Col className='three' lg={3} md={4} sm={6} xs={12} >    </Col>
         <Col className='four' lg={3} md={4} sm={6} xs={12} > </Col>
      </Row>
-
-{/* <Container>
-  <Row>
-    <Col><div className='one'></div></Col>
-    <Col><div className='two'></div></Col>
-  </Row>
-  <Row>
-    <Col><div className='three'></div></Col>
-    <Col><div className='four'></div></Col>
-    </Row>
-</Container> */}
+        
     </div>
   );
 }
